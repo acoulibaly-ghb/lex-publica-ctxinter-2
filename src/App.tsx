b4037201200d988320ad28d310a5ee99d1b4dc06
acoulibaly-ghb <abdoulaye.coulibaly@gmail.com>
2025-11-28 18:32:43 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ const App: React.FC = () => {
               Lex publica IA <span className="text-4xl font-extrabold tracking-tight text-slate-400 sm:text-5xl mb-2"> by </span><span className="text-indigo-600">Coulibaly</span>
             </h1>
             <p className="text-lg text-slate-600 max-w-2xl mx-auto leading-relaxed">
-              Votre assistant expert en droit administratif.
-              <span className="block text-sm text-slate-400 mt-1 font-medium">Actes unilatéraux • Police • Service Public</span>
+              Votre assistant expert en droit du contentieux international.
+              <span className="block text-sm text-slate-400 mt-1 font-medium">Introduction générale • Première partie • Seconde partie</span>
             </p>
           </div>
         </header>
